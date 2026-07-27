b7b798ff7124d0a26172f7155a3b1a250816b0e7
Juanjo González <juanjo96developer@gmail.com>
2026-07-27 12:22:26 +0200

    feat(auth): add public auth runtime fields

--- public/config.js
+++ public/config.js
@@ -1,5 +1,8 @@
 window.__MINUTO106_CONFIG__ = {
   apiBaseUrl: 'https://imtitjwgiemlaabpioed.supabase.co/functions/v1/game-api',
+  accountAuthApiUrl: 'https://imtitjwgiemlaabpioed.supabase.co/functions/v1/account-auth',
+  supabaseUrl: 'https://imtitjwgiemlaabpioed.supabase.co',
+  supabasePublishableKey: '',
   turnstileSiteKey: '',
   googleAnalyticsId: '',
   adSenseClient: '',
