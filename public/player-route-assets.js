9422c55b4c372eaee3c06cd8ce374915ecbeacb0
Juanjo González <juanjo96developer@gmail.com>
2026-07-31 00:47:29 +0200

    refactor(profile): remove redundant route asset module

--- public/player-route-assets.js
+++
@@ -1,6 +0,0 @@
-(() => {
-  const appBaseUrl = globalThis.Minuto106PlayerUI?.appBaseUrl?.();
-  const favicon = globalThis.document?.querySelector?.('link[rel~="icon"]');
-  if (!appBaseUrl || !favicon) return;
-  favicon.href = new URL('assets/favicon.svg', appBaseUrl).toString();
-})();
